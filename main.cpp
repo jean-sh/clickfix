d364c7fffb15a9e61a408ed04c9253ad92dbd972
Jean Vincent <jean.vincent@posteo.net>
2017-06-17 13:49:50 +0200

    Added automatic search for uinput and the event mouse + corrected incorrect event loop

--- main.cpp
+++ main.cpp
@@ -5,14 +5,16 @@
 #include <cstring>
 #include <linux/input.h>
 #include <linux/uinput.h>
+#include <experimental/filesystem>
 
-// TODO: Profile << vs printf
+// TODO: Profile performance
 
 const std::string ERROR_IOCTL = "error: ioctl";
 const std::string ERROR_OPEN = "error: open";
 const std::string ERROR_WRITE = "error: write";
+const int TRIGGER_THRESHOLD = 90000;
 	
-void die(const std::string message)
+void die(const std::string& message)
 {
 	std::cerr << message << "\n";
 	exit(EXIT_FAILURE);
@@ -20,15 +22,28 @@ void die(const std::string message)
 
 int create_uinput()
 {
-	int fd;
-    struct uinput_user_dev uidev;
+	namespace fs = std::experimental::filesystem;
+	using std::string;
 	
-    fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
-
+	/* Find where uinput is */
+	const fs::path dev_uinput = "/dev/uinput";
+	const fs::path dev_input_uinput = "/dev/input/uinput";
+	string uinput_path;
+	
+	if (fs::exists(dev_uinput)) {
+		uinput_path = dev_uinput.string();
+	} else if (fs::exists(dev_input_uinput)) {
+		uinput_path = dev_input_uinput.string();
+	}
+	std::cout << "Found uinput device: " << uinput_path << "\n";
+	
+	/* Open uinput */
+    const int fd = open(uinput_path.c_str(), O_WRONLY | O_NONBLOCK);
 	if (fd < 0) {
         die(ERROR_OPEN);
 	}
 
+	/* Set all bits we need for a mouse */
     if (ioctl(fd, UI_SET_EVBIT, EV_KEY) < 0) {
         die(ERROR_IOCTL);
 	}
@@ -82,14 +97,24 @@ int create_uinput()
         die(ERROR_IOCTL);
 	}
 	
-	if (ioctl(fd, UI_SET_KEYBIT, REL_WHEEL) < 0) {
+	if (ioctl(fd, UI_SET_RELBIT, REL_WHEEL) < 0) {
 		die(ERROR_IOCTL);
 	}
 	
-	if (ioctl(fd, UI_SET_KEYBIT, REL_HWHEEL) < 0) {
+	if (ioctl(fd, UI_SET_RELBIT, REL_HWHEEL) < 0) {
+		die(ERROR_IOCTL);
+	}
+	
+	if (ioctl(fd, UI_SET_RELBIT, REL_MISC) < 0) {
+		die(ERROR_IOCTL);
+	}
+	
+	if (ioctl(fd, UI_SET_RELBIT, REL_MAX) < 0) {
 		die(ERROR_IOCTL);
 	}
 
+	/* Create the device */
+	struct uinput_user_dev uidev;
     memset(&uidev, 0, sizeof(uidev));
     snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "click-fixer");
     uidev.id.bustype = BUS_USB;
@@ -104,7 +129,7 @@ int create_uinput()
     if (ioctl(fd, UI_DEV_CREATE) < 0) {
         die(ERROR_IOCTL);
 	}
-	
+
 	return fd;
 }
 
@@ -113,11 +138,11 @@ void destroy_uinput(const int fd)
 	if (ioctl(fd, UI_DEV_DESTROY) < 0) {
         die(ERROR_IOCTL);
 	}
-	
+
 	close(fd);
 }
 
-void inject_event(const int fd, input_event& event)
+void inject_event(const int fd, const input_event& event)
 {
 	/* Writing the event to uinput */
 	input_event ev;
@@ -125,14 +150,11 @@ void inject_event(const int fd, input_event& event)
 	ev.type = event.type;
 	ev.code = event.code;
 	ev.value = event.value;
-	//ev.time = event.time;
-	
-	//std::cout << "ev.code: " << ev.code << " ev.value: " << ev.value << "\n";
-	
+
 	if (write(fd, &ev, sizeof(input_event)) < 0) {
 		std::cerr << ERROR_OPEN << "\n";
 	}
-	
+
 	/* sync */
 	memset(&ev, 0, sizeof(input_event));
 	ev.type = EV_SYN;
@@ -150,25 +172,30 @@ int get_timeval_usec(const struct timeval& t)
 
 int main(int argc, char* argv[])
 {
-	
-	
-	if (argc != 2 || !strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
-		std::cerr << "\n";
-		std::cerr << "Usage: " << argv[0] << " [..-h | --help ]\n";
-		std::cerr << "       " << argv[0] << " /dev/input/event0\n";
-		std::cerr << "\n";
-		std::cerr << "This example program describes mouse events.\n";
-		std::cerr << "\n";
-		return 0;
+	namespace fs = std::experimental::filesystem;
+	using std::string;
+
+	/* Find the path to the event mouse */
+	string device_path;
+	const string dev_input = "/dev/input";
+	fs::directory_iterator it(dev_input + "/by-path");
+	for (auto f : it) {
+		if (f.path().string().find("event-mouse") != string::npos) {
+			string tmp;
+			tmp = fs::read_symlink(f.path()).string();
+			device_path = dev_input + tmp.substr(2);
+			std::cout << "Found event mouse: " << device_path << "\n";
+			break;
+		}
 	}
 
 	/* Open the specified device. */
 	int fd;
 	do {
-		fd = open(argv[1], O_RDONLY);
+		fd = open(device_path.c_str(), O_RDONLY);
 	} while (fd == -1 && errno == EINTR);
 	if (fd == -1) {
-		std::cerr << argv[1] << ": " << strerror(errno) << ".\n";
+		std::cerr << device_path << ": " << strerror(errno) << ".\n";
 		return 1;
 	}
 
@@ -176,15 +203,6 @@ int main(int argc, char* argv[])
 	char device_name[64];
 	memset(device_name, 0, sizeof device_name);
 	ioctl(fd, EVIOCGNAME(sizeof device_name - 1), device_name); 
-
-	/* Obtain real, absolute path to the device. */
-	char* truedev;
-	truedev = canonicalize_file_name(argv[1]);
-	if (truedev != NULL) {
-		std::cerr << "Reading from " << truedev << " (" << argv[1] << ")\n";
-	} else {
-		std::cerr << "Reading from " << argv[1] << "\n";
-	}
 
 	if (strlen(device_name) > 0) {
 		std::cerr << "EVIOCGNAME ioctl reports device is named '" << device_name << "'.\n";
@@ -196,22 +214,23 @@ int main(int argc, char* argv[])
 	} else {
 		std::cerr << "Failed to grab device (" << strerror(errno) << ").\n";
 	}
-	
+
 	// Create the virtual device to inject events into.
-	// Part of the creation is asynchrounous
-	// so we sleep for 500 ms to let it finish.
-	int fd_uinput = create_uinput();
-	usleep(500000);
-	
+
+	const int fd_uinput = create_uinput();
+	usleep(500000);		// Part of the creation is asynchrounous so we sleep for 500 ms to let it finish.
+
 	/* Intercept event loop */
 	input_event ev;
 	ssize_t bytes_read;
 	struct prev_btn_left_event {
-		int pressed = 0;
+		// timestamp in microseconds
+		int pressed = 0;	
 		int released = 0;
 	} prev_event;
+	bool ignore;
 	while (1) {
-		
+
 		// TODO: better flow
 		bytes_read = read(fd, &ev, sizeof ev);
 		if (bytes_read == -1) {
@@ -230,36 +249,41 @@ int main(int argc, char* argv[])
 			} else {
 				if (bytes_read != sizeof ev) {
 					/* This should never occur; input driver or kernel bug. */
-					std::cerr << argv[1] << ": Invalid event (length " << (int)bytes_read << ", expected " << (int)sizeof ev<< ")\n";
+					std::cerr << argv[1] << ": Invalid event (length "
+						<< (int)bytes_read << ", expected " << (int)sizeof ev<< ")\n";
 					/* We just ignore those, and wait for next event. */
 					continue;
 				}
 			}
 		}
-		
-		std::cout << "CODE:	" << ev.code << "\n";
-		
+
+		//std::cout << "CODE:	" << ev.code << "\n";
+
 		// TODO: better flow?
+		ignore = false;
 		if (ev.code == BTN_LEFT) {
 			if (ev.value == 1) {
-				if (get_timeval_usec(ev.time) - prev_event.pressed < 90000) {
-					std::cout << "Double pressed\n";
-					continue;
+				if (get_timeval_usec(ev.time) - prev_event.pressed < TRIGGER_THRESHOLD) {
+					//std::cout << "Double pressed\n";
+					ignore = true;
 				}
 				prev_event.pressed = get_timeval_usec(ev.time);
 			} else if (ev.value == 0) {
-				if (get_timeval_usec(ev.time) - prev_event.released < 90000) {
-					std::cout << "Double released\n";
-					continue;
+				if (get_timeval_usec(ev.time) - prev_event.released < TRIGGER_THRESHOLD) {
+					//std::cout << "Double released\n";
+					ignore = true;
 				}
 				prev_event.released = get_timeval_usec(ev.time);
 			}
 		}
-		
-		inject_event(fd_uinput, ev);
+
+		if (not ignore) {
+			inject_event(fd_uinput, ev);
+		}
 	}
 
+	destroy_uinput(fd_uinput);
 	close(fd);
-	
+
 	return 0;
 }
